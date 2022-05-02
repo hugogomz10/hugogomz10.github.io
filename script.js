13024302593b2a3b9cdd40337f914c8d8db551de
Hugo Gomez <72419821+hugogomz10@users.noreply.github.com>
2022-05-02 17:15:49 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+//Funciones para primer par de cartas
+function mostrar(){
+    document.getElementById("img").style.display = "block";
+}
+
+function ocultar(){
+    document.getElementById("img").style.display = "none";
+}
+
+//Funciones para segundo par de cartas
+function mostrar2(){
+    document.getElementById("img2").style.display = "block";
+}
+
+function ocultar2(){
+    document.getElementById("img2").style.display = "none";
+}
+
+//Funciones para tercer par de cartas
+function mostrar3(){
+    document.getElementById("img3").style.display = "block";
+}
+
+function ocultar3(){
+    document.getElementById("img3").style.display = "none";
+}
+
+//Funciones para cuarto par de cartas
+function mostrar4(){
+    document.getElementById("img4").style.display = "block";
+}
+
+function ocultar4(){
+    document.getElementById("img4").style.display = "none";
+}
+
+//Funciones para quinto par de cartas
+function mostrar5(){
+    document.getElementById("img5").style.display = "block";
+}
+
+function ocultar5(){
+    document.getElementById("img5").style.display = "none";
+}
