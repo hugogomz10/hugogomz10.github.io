6472a4624458d951e40b787febf376a4e63436c4
Hugo Gomez <72419821+hugogomz10@users.noreply.github.com>
2022-05-03 19:59:51 -0500

    Update repository

--- script.js
+++ script.js
@@ -1,44 +1,94 @@
-//Funciones para primer par de cartas
-function mostrar(){
-    document.getElementById("img").style.display = "block";
-}
+///Primera carta
+var btnAbrirPopup = document.getElementById('btntap'),
+    overlay = document.getElementById('overlay'),
+    popup = document.getElementById('popup'),
+    btnCerrarPopup = document.getElementById('btncerrar'),
+    ocultarCarta = document.getElementById('btntap');
 
-function ocultar(){
-    document.getElementById("img").style.display = "none";
-}
+btnAbrirPopup.addEventListener('click', function(){
+    overlay.classList.add('active');
+    popup.classList.add('active');
+    ocultarCarta.classList.add('active');
+});
 
-//Funciones para segundo par de cartas
-function mostrar2(){
-    document.getElementById("img2").style.display = "block";
-}
+btnCerrarPopup.addEventListener('click', function(){
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
+    ocultarCarta.classList.remove('active');
+});
 
-function ocultar2(){
-    document.getElementById("img2").style.display = "none";
-}
+///Segunda carta
+var btnAbrirPopup2 = document.getElementById('btntap2'),
+    overlay2 = document.getElementById('overlay2'),
+    popup2 = document.getElementById('popup2'),
+    btnCerrarPopup2 = document.getElementById('btncerrar2'),
+    ocultarCarta2 = document.getElementById('btntap2');
 
-//Funciones para tercer par de cartas
-function mostrar3(){
-    document.getElementById("img3").style.display = "block";
-}
+btnAbrirPopup2.addEventListener('click', function(){
+    overlay2.classList.add('active');
+    popup2.classList.add('active');
+    ocultarCarta2.classList.add('active');
+});
 
-function ocultar3(){
-    document.getElementById("img3").style.display = "none";
-}
+btnCerrarPopup2.addEventListener('click', function(){
+    overlay2.classList.remove('active');
+    popup2.classList.remove('active');
+    ocultarCarta2.classList.remove('active');
+});
 
-//Funciones para cuarto par de cartas
-function mostrar4(){
-    document.getElementById("img4").style.display = "block";
-}
+///Tercera carta
+var btnAbrirPopup3 = document.getElementById('btntap3'),
+    overlay3 = document.getElementById('overlay3'),
+    popup3 = document.getElementById('popup3'),
+    btnCerrarPopup3 = document.getElementById('btncerrar3'),
+    ocultarCarta3 = document.getElementById('btntap3');
 
-function ocultar4(){
-    document.getElementById("img4").style.display = "none";
-}
+btnAbrirPopup3.addEventListener('click', function(){
+    overlay3.classList.add('active');
+    popup3.classList.add('active');
+    ocultarCarta3.classList.add('active');
+});
 
-//Funciones para quinto par de cartas
-function mostrar5(){
-    document.getElementById("img5").style.display = "block";
-}
+btnCerrarPopup3.addEventListener('click', function(){
+    overlay3.classList.remove('active');
+    popup3.classList.remove('active');
+    ocultarCarta3.classList.remove('active');
+});
 
-function ocultar5(){
-    document.getElementById("img5").style.display = "none";
-}
+///Tercera carta
+var btnAbrirPopup4 = document.getElementById('btntap4'),
+    overlay4 = document.getElementById('overlay4'),
+    popup4 = document.getElementById('popup4'),
+    btnCerrarPopup4 = document.getElementById('btncerrar4'),
+    ocultarCarta4 = document.getElementById('btntap4');
+
+btnAbrirPopup4.addEventListener('click', function(){
+    overlay4.classList.add('active');
+    popup4.classList.add('active');
+    ocultarCarta4.classList.add('active');
+});
+
+btnCerrarPopup4.addEventListener('click', function(){
+    overlay4.classList.remove('active');
+    popup4.classList.remove('active');
+    ocultarCarta4.classList.remove('active');
+});
+
+///Tercera carta
+var btnAbrirPopup5 = document.getElementById('btntap5'),
+    overlay5 = document.getElementById('overlay5'),
+    popup5 = document.getElementById('popup5'),
+    btnCerrarPopup5 = document.getElementById('btncerrar5'),
+    ocultarCarta5 = document.getElementById('btntap5');
+
+btnAbrirPopup5.addEventListener('click', function(){
+    overlay5.classList.add('active');
+    popup5.classList.add('active');
+    ocultarCarta5.classList.add('active');
+});
+
+btnCerrarPopup5.addEventListener('click', function(){
+    overlay5.classList.remove('active');
+    popup5.classList.remove('active');
+    ocultarCarta5.classList.remove('active');
+});
